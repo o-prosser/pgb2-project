24f95842cea83cf7b124bb842330f125658a3b4a
Renokkusu <whlennox12@gmail.com>
2026-04-26 18:16:50 +0100

    added left/right handedness and also flipped camera

--- camera.py
+++ camera.py
@@ -2,13 +2,18 @@ import cv2 as c
 import hand_tracking as ht
 
 def startCapture(cam):
+
+    frame_width = cam.get(c.CAP_PROP_FRAME_WIDTH)
+    frame_height = cam.get(c.CAP_PROP_FRAME_HEIGHT)
+
     while True:
         x,frame = cam.read()
+        frame = c.flip(frame, 1)
         if not x:
             break
 
         rgb = c.cvtColor(frame,c.COLOR_BGR2RGB)
-        ht.checkHands(frame,rgb)
+        ht.checkHands(frame,rgb,frame_width,frame_height)
 
         c.imshow("Hand",frame)
 
--- hand_tracking.py
+++ hand_tracking.py
@@ -1,15 +1,25 @@
 import mediapipe as mp
 
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands(max_num_hands = 1)
-mp_draw = mp.solution.drawing_utils
+hands = mp_hands.Hands(max_num_hands=2)
+mp_draw = mp.solutions.drawing_utils
 
-def checkHands(frame,rgb):
+def checkHands(frame, rgb,frame_width,frame_height):
     results = hands.process(rgb)
 
     if results.multi_hand_landmarks:
-        hand = results.multi_hand_landmarks[0]
+        for landmarks, handedness in zip(results.multi_hand_landmarks, results.multi_handedness):
+            mp_draw.draw_landmarks(frame, landmarks, mp_hands.HAND_CONNECTIONS)
+            
+            label = handedness.classification[0].label
+            if label == "Left":
+                getHandData(landmarks, frame_width, frame_height)
 
-        mp_draw.draw_landmarks(frame, hand, mp_hands.HAND_CONNECTIONS)
 
+def getHandData(hand_landmarks,frame_width,frame_height):
+    hand_landmarks_list=[]
+    for lm in hand_landmarks.landmark:
+        hand_landmarks_list.extend([lm.x,lm.y,lm.z])
+
+    print(hand_landmarks_list)
 
