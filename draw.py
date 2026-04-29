3468bc1c3704e6f6c96ad8fd21545504e1be61e8
Owen Prosser <owenprosser@icloud.com>
2026-04-29 14:33:41 +0100

    Add new colour for drawing with 3rd finger

--- draw.py
+++ draw.py
@@ -13,6 +13,22 @@ def handleDraw(frame, gesture_l, gesture_r, index_pos, draw_points):
         if gesture_r == "one":
             draw_points.append([(x, y),col])
 
+    if gesture_l == "two":
+        draw_points = draw_points.copy()
+
+        for point in draw_points:
+            if abs(point[0][0] - x) < 10 and abs(point[0][1] -y) < 10:
+                draw_points.remove(point)
+
+    if gesture_l == "three":
+
+        hue = int(index_pos.x * 179)
+        hsv_col = n.uint8([[[hue, 255, 255]]])
+        bgr_col = c.cvtColor(hsv_col, c.COLOR_HSV2BGR)
+        col = tuple(int(x) for x in bgr_col[0][0])
+
+        c.circle(frame, (x,y), 5, col, -1)
+
     if gesture_l == "zero" and gesture_r == "open":
         return removeCanvas()
     
